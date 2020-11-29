8920a35adba4d6eadd3cda0ac9890fa25e665a7e
elefstathi <elefstathi@gmail.com>
2020-11-29 14:56:22 +0200

    modal for deletetion added

--- src/components/CoursesDetails.js
+++ src/components/CoursesDetails.js
@@ -1,8 +1,8 @@
-import { Typography, Row, Layout, Spin, Col } from "antd";
+import { Typography, Row, Layout, Spin, Modal, Button} from "antd";
 import React, { useState } from "react";
 import CheckOutlinedIcon from "@material-ui/icons/CheckOutlined";
 import ClearOutlinedIcon from "@material-ui/icons/ClearOutlined";
-import API_BASE_URL, {API_INSTRUCTORS, API_COURSES} from "../api/BaseApi";
+import API_BASE_URL, { API_INSTRUCTORS, API_COURSES } from "../api/BaseApi";
 import { green, red, blue } from "@material-ui/core/colors";
 import { useHistory } from "react-router-dom";
 import { useApiCall } from "../hooks/useApiCall";
@@ -12,24 +12,39 @@ const { Content, Footer } = Layout;
 
 const CoursesDetails = ({ course }) => {
   const [error, setError] = useState(null);
+  const [ show, setModal] = useState(false);
   const history = useHistory();
-  const { title, id, description, duration, dates, imagePath, instructors, open, price } = course;
-  const { sendData } = useApiCall(`${API_COURSES}/${id}`, 'DELETE');
+  const {
+    title,
+    id,
+    description,
+    duration,
+    dates,
+    imagePath,
+    instructors,
+    open,
+    price,
+  } = course;
+  const { sendData } = useApiCall(`${API_COURSES}/${id}`, "DELETE");
   const { data: respInstructors, isLoading } = useApiCall(API_INSTRUCTORS);
 
   const handleEdit = () => {
-    history.push('/add_new_course', course);
-  } 
+    history.push("/add_new_course", course);
+  };
 
   const handleDelete = async () => {
     try {
       const resp = await sendData();
       history.push("/courses");
     } catch {
-      setError('ERROR IN DELETE');
-      alert(error)
+      setError("ERROR IN DELETE");
+      alert(error);
     }
-  }
+  };
+
+  const showModal= (e) => { setModal(true) };
+
+  const handleCancel = (e) => { setModal(false) };
 
   const renderCourseDetail = () => {
     return (
@@ -46,11 +61,16 @@ const CoursesDetails = ({ course }) => {
                 justifyContent: "center",
               }}
             >
-             { imagePath && (<img
-                style={{ width: "50%", height: "40%" }}
-                src={imagePath.includes('http') ? imagePath : `${API_BASE_URL}${imagePath}`}
-              />
-            )} 
+              {imagePath && (
+                <img
+                  style={{ width: "50%", height: "40%" }}
+                  src={
+                    imagePath.includes("http")
+                      ? imagePath
+                      : `${API_BASE_URL}${imagePath}`
+                  }
+                />
+              )}
             </Content>
             <Footer>
               <Row style={rowStyle}>
@@ -78,17 +98,32 @@ const CoursesDetails = ({ course }) => {
                 <button style={editStyle} onClick={handleEdit}>
                   Edit
                 </button>
-                <button
-                  style={deleteStyle}
-                  onClick={handleDelete}
-                >
+                <button style={deleteStyle} onClick={showModal}>
                   Delete
                 </button>
               </Row>
               <Title level={2}>Instructors</Title>
-                {respInstructors && instructors.map((id) => respInstructors.filter((item) => item.id === id))
+              {respInstructors &&
+                instructors
+                  .map((id) => respInstructors.filter((item) => item.id === id))
                   .map((instructor) => renderInstructors(instructor[0]))}
             </Footer>
+            <Modal
+              title="Delete Course"
+              visible={show}
+              onOk={handleDelete}
+              onCancel={handleCancel}
+              footer={[
+                <Button key="back" style={{background: "#3b3b3b", color: '#fff'}} onClick={handleCancel}>
+                  Cancel
+                </Button>,
+                <Button key="submit" style={{background: blue[900], color: "#fff"}} onClick={handleDelete}>
+                  Delete
+               </Button>
+              ]}
+            >
+              <Text>Are you sure you want to delete "{title}" course?</Text>
+            </Modal>
           </Layout>
         </div>
       </>
@@ -97,14 +132,17 @@ const CoursesDetails = ({ course }) => {
 
   const renderInstructors = (instructor) => {
     return (
-        <div key={instructor.id}>
+      <div key={instructor.id}>
         <Title level={3}>
-            {instructor.name.first} {instructor.name.last} ({instructor.dob})
+          {instructor.name.first} {instructor.name.last} ({instructor.dob})
         </Title>
         <Text>Email: {instructor.email} | </Text>
-        <a target="_blank" href={instructor.linkedin}>Linkedin</a><br/>
+        <a target="_blank" href={instructor.linkedin}>
+          Linkedin
+        </a>
+        <br />
         <Text>{instructor.bio}</Text>
-        </div>
+      </div>
     );
   };
 
@@ -114,5 +152,9 @@ const CoursesDetails = ({ course }) => {
 export default CoursesDetails;
 
 const editStyle = { color: "#fff", background: blue[900], borderRadius: "5px" };
-const deleteStyle = { color: "#fff", background: red[500], borderRadius: "5px" };
+const deleteStyle = {
+  color: "#fff",
+  background: red[500],
+  borderRadius: "5px",
+};
 const rowStyle = { flex: 1, justifyContent: "space-between", marginRight: 5 };
